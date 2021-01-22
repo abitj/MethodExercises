a17b9f1c547753bfaaa5396348fa4710a9b3377f
Abit Jurkuch <66223142+abitj@users.noreply.github.com>
2021-01-22 15:36:36 -0600

    Add files via upload

---
+++ MethodExercises/MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your bull name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your bull color?");
+             var color = Console.ReadLine();
+
+             Console.WriteLine("How old is your bull?");
+             var bullAge = Console.ReadLine();
+
+             Console.WriteLine("Where does your bull sleep?");
+             var sleepPlace = Console.ReadLine();
+            
+
+            Console.WriteLine($"My bull name is {name} and it 's {color}. It turns {bullAge} in January. My bull sleeps in {sleepPlace}");
+
+        }
+    }
+}
---
+++ MethodExercises/MethodExercise1/MethodExercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MethodExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine(Sum(15, 60));
+            Console.WriteLine(Substract(10, 6));
+            Console.WriteLine(Multiply(5, 60));
+            Console.WriteLine(Divide(15, 3));
+         
+        }
+        public static int Sum(int a, int b)
+        {
+            int answer = a + b;
+            return answer;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            int answer = a - b;
+            return answer;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            int answer = a * b;
+            return answer;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            int answer = a / b;
+            return answer;
+        }
+    }
+}
